696d5e4c600fbac254168589d782e0d8b7ef2d11
Luke Tytor <Luke@DeepThought-PC.(none)>
2012-04-17 15:57:25 -0700

    luke

---
+++ iBuySwing/src/ibuyswing/ChangeMenu.java
@@ -0,0 +1,109 @@
+package ibuyswing;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.session.WebAuthSession;
+
+
+public class ChangeMenu extends JFrame implements ActionListener{
+	private DropboxAPI<WebAuthSession> mDBApi;
+	private JTextField entry = new JTextField();
+	private JButton cancel = new JButton("Back");
+	private JButton update = new JButton("Update");
+	private String user, filename;
+		
+	public ChangeMenu(String user, String filename, DropboxAPI<WebAuthSession> mDBApi) 
+	{
+		super("iBuy - Change Menu");
+		this.user = user;
+		this.filename = filename;
+		this.mDBApi = mDBApi;
+		
+		setLayout(new GridLayout(3,1));
+		setSize(500,300);
+		
+		entry.setText(filename);
+        cancel.addActionListener(this);
+        update.addActionListener(this);
+        add(entry);
+        add(update);
+	    add(cancel);
+		
+        setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == cancel)
+		{
+			new addItemFrame(user, filename, mDBApi);
+			setVisible(false);
+			dispose();
+		}
+		if(e.getSource() == update)
+		{
+			String newName = entry.getText();
+			
+			//Checks if name is taken
+			String listNames = Global.getFile(mDBApi, "/" + user + "/lists.txt");
+			StringTokenizer scanner = new StringTokenizer(listNames);
+			boolean isTaken = false;
+			while(scanner.hasMoreTokens())
+  			{
+  				String listname = scanner.nextToken();
+  				scanner.nextToken();
+  				scanner.nextToken();
+  				if(listname.equals(Global.toFileName(newName)))
+  				{
+  					isTaken = true;
+  				}
+  			}
+			if(isTaken)
+			{
+				System.out.println("List name is taken.");
+			}
+			else
+			{
+				//Read contents of file and writes to new file
+				String contents = Global.getFile(mDBApi, "/" + user + "/" + Global.toFileName(filename) + ".txt");
+				Global.putFile(mDBApi, "/" + user + "/" + Global.toFileName(entry.getText()) + ".txt", contents);
+				
+				//Delete old file
+				Global.delete(mDBApi, "/" + user + "/" + Global.toFileName(filename) + ".txt");
+				
+				//Finds filename in list and changes it
+				StringTokenizer st = new StringTokenizer(Global.getFile(mDBApi, "/" + user + "/lists.txt"));
+				String newList = "";
+				while(st.hasMoreTokens())
+				{
+					String token = st.nextToken();
+					String token2 = st.nextToken();
+					String token3 = st.nextToken();
+					if(token.equals(Global.toFileName(filename)))
+					{
+						Date d = new Date(System.currentTimeMillis());
+						newList += Global.toFileName(newName) + "\t" + token2 + "\t" + Global.toFileName(d.toString())  + "\n";
+					}
+					else
+						newList += token + "\t" + token2 + "\t" + token3 + "\n";
+				}
+				Global.putFileOverwrite(mDBApi, "/" + user + "/lists.txt", newList);
+				
+				//Returns to List
+				new addItemFrame(user, newName, mDBApi);
+				setVisible(false);
+				dispose();
+			}
+		}
+	}
+		
+}
---
+++ iBuySwing/src/ibuyswing/DeleteMenu.java
@@ -0,0 +1,94 @@
+package ibuyswing;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.exception.DropboxException;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.WebAuthSession;
+
+
+public class DeleteMenu extends JFrame implements ActionListener{
+	private DropboxAPI<WebAuthSession> mDBApi;
+	private LinkedList<JButton> list = new LinkedList<JButton>();
+	private JButton cancel = new JButton("Back");
+	private String user;
+	
+	public DeleteMenu(String user, DropboxAPI<WebAuthSession> mDBApi) 
+	{
+		super("iBuy - Delete Menu");
+		this.user = user;
+		this.mDBApi = mDBApi;
+		
+		//Reads users list of lists
+		list.clear();
+	    StringTokenizer st = new StringTokenizer(Global.getFile(mDBApi, "/"+user+"/lists.txt"));
+		while(st.hasMoreTokens())
+		{
+			JButton j = new JButton(Global.readFileName(st.nextToken()));
+			st.nextToken();
+			st.nextToken();
+			j.addActionListener(this);
+			list.add(j);
+		}
+		setLayout(new GridLayout(list.size()+1,1));
+		setSize(500, Math.min((200 + 100*list.size()), 1000));
+		for(int i = 0; i < list.size(); i++)
+		{
+		    add(list.get(i));
+		}
+        
+        cancel.addActionListener(this);
+	    add(cancel);
+		
+	    setContentPane(new JScrollPane(getContentPane()));
+        setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == cancel)
+		{
+			new mainMenuFrame(user, mDBApi);
+			setVisible(false);
+			dispose();
+		}
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(e.getSource() == list.get(i))
+			{
+				//Delete file
+				Global.delete(mDBApi, "/" + user + "/" + Global.toFileName(list.get(i).getText()) + ".txt");
+				StringTokenizer st = new StringTokenizer(Global.getFile(mDBApi, "/" + user + "/lists.txt"));
+				String newList = "";
+				//Finds filename in list and removes it
+				while(st.hasMoreTokens())
+				{
+					String token = st.nextToken();
+					if(!token.equals(Global.toFileName(list.get(i).getText())))
+						newList += token + "\t" + st.nextToken() + "\t" + st.nextToken() + "\n";
+					else
+					{
+						st.nextToken();
+						st.nextToken();
+					}
+				}
+				Global.putFileOverwrite(mDBApi, "/" + user + "/lists.txt", newList);
+				
+				//"Refreshes" window
+				new DeleteMenu(user, mDBApi);
+				setVisible(false);
+				dispose();
+			}
+		}
+	}
+}
